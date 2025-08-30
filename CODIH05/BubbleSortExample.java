16032aae30338ad4b9fb2f257eb65f58616d1bd2
Thosaam <thomassaam@gmx.de>
2025-08-30 19:38:14 +0200

    BubbleSortExample fixed, Algorithmus gibt die Liste zurück wenn nichts vertauscht wurde

--- CODIH05/BubbleSortExample.java
+++ CODIH05/BubbleSortExample.java
@@ -2,25 +2,27 @@ public class BubbleSortExample {
     static void bubbleSort(int[] arr) {
 
         int n = arr.length;
-
         int temp = 0;
+        boolean getauscht;
 
         for(int i=0; i < n; i++){
+             getauscht= false;
 
             for(int j=1; j < (n-i); j++){
-
                 if(arr[j-1] > arr[j]){
-
-//swap elements
+                 //swap elements
 
                     temp = arr[j-1];
 
                     arr[j-1] = arr[j];
 
                     arr[j] = temp;
-
+                    getauscht=true;
+                    }
                 }
-
+            if (!getauscht) {
+            break;
+            }
             }
 
         }
